31c5dd69bbe5785959c71469d2005e4742d1880c
RedAvocado <minhcuong2922004@gmail.com>
2025-07-17 08:16:22 +0700

    small: git ignore

--- Monkey Blogging/monkey-blogging/src/main.tsx
+++ Monkey Blogging/monkey-blogging/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { theme } from "./utils/constants.ts";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/GlobalStyles.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <GlobalStyles></GlobalStyles>
+            <BrowserRouter>
+                <App />
+                <ToastContainer />
+            </BrowserRouter>
+        </ThemeProvider>
+    </StrictMode>
+);
